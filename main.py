07acfcc60fd884776327bf290dd536bf5b070715
SieSiongWong <lawsvin@gmail.com>
2023-09-05 16:39:09 -0400

    test6

--- main.py
+++ main.py
@@ -5,8 +5,9 @@
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script. Test1234
+    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Test123')
 
 
 # Press the green button in the gutter to run the script.
